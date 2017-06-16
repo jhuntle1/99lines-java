23628baab70ed4fb3c60ea1a5b32d0a933dcc2c0
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-16 10:36:15 -0500

    first commit

---
+++ 99lines.js
@@ -0,0 +1,26 @@
+var jobs = [
+    {
+        companyName:'mcDonalds',
+        position: 'chef',
+        description: 'i made hamburgers'
+
+    },
+    {
+        companyName: 'burger king',
+        position: 'sous chef',
+        descriotion: 'i hand out cheeseburgers'
+    },
+    {
+        commpanyName: 'Taco bell',
+        position: 'sauceier',
+        description: 'i hand out sauce'
+    }
+    
+];
+
+function displayPosition(jobsArray) {
+    for (var i = 0; i < jobsArray.length;);
+}
+
+
+var jobs 
