ff3124984449c20459c4fde07440cacb39d13db7
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-16 13:54:32 -0500

    no srsly now the variable's right

--- 99lines.js
+++ 99lines.js
@@ -2,7 +2,7 @@ var friends = ['Bean ', 'Pierre ', 'Miesha ', 'Brandon ', 'Rayshawn '];
 for (x=0;x<friends.length; x++){
     for (i=99; i > 1; i--) {
 
-        console.log([i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
+        console.log([++i] + ' lines of code in the file,' + ' ' + [i]+ ' ' + 'lines of code,' + ' ' + friends[x] + ' ' + 'strikes one out, clears it all out, ' + [--i] + ' lines of code on the wall');
     
     }
     console.log([i] + ' line of code in the file,' + ' ' + [i] +' line of code, ' + friends[x] + ' strikes one out, clears it all out, ' +[i] + ' line of code on the wall');
